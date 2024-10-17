df96947e616a7288f72668ccbfa8d6d6948ed58e
devhalimah <abdulazeezhaleemah@gmail.com>
2024-10-17 21:18:50 +0100

    set function fixed

---
+++ Storage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Storage{
+    //The uninitialized variable that will hold the number
+    uint256 input;
+    //The get function that retrieves the number
+    function get() public view returns(uint256) {
+        return input;
+    }
+    //The set function that takes the inputted number
+    function set(uint256 _input) public {
+        input = _input;
+    }
+}
---
+++ variables.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract DefiningVariables {
+    string name = "this is a name";
+    uint256 number = 56;
+    bool answer = true;
+    int NegativeNumber = -45;
+    address WalletAddress = 0x742d35Cc6634C0532925a3b844Bc454e4438f44e;
+}
